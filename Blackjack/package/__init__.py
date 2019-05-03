1c8fa6f0d647a0e071115c1e332e04642dee1ab5
micascheid <mica.linscheid@western.edu>
2019-05-03 12:46:02 -0600

    Blackjack.package

--- Blackjack/package/__init__.py
+++ Blackjack/package/__init__.py
@@ -15,4 +15,4 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-from package.routes import AccountRoutes, GameplayRoutes
+from Blackjack.package.routes import AccountRoutes, GameplayRoutes
