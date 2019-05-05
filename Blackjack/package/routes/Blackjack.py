ade4bdc094f111dccdbabc94f56261fd48424af3
micascheid <mica.linscheid@western.edu>
2019-05-05 08:47:22 -0600

    heroku test

--- Blackjack/package/routes/Blackjack.py
+++ Blackjack/package/routes/Blackjack.py
@@ -1,10 +1,10 @@
-from package import app, db
+from Blackjack.package import app, db
 from flask_login import login_required
 from flask import render_template, redirect, url_for, jsonify, make_response, json, request
 from flask_login import current_user
-from package.Databases import database_model as dm
-from package.Databases.database_model import User, Deck, Player, Table
-from package.routes.Results import HitResult, SplitResult, RoundResult
+from Blackjack.package.Databases import database_model as dm
+from Blackjack.package.Databases.database_model import User, Deck, Player, Table
+from Blackjack.package.routes.Results import HitResult, SplitResult, RoundResult
 
 class DoubleDown:
     def __init__(self, bet, funds):
